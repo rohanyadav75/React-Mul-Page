070e479dfcd4490c65949a378565b2b0128f141d
Rohan Yadav <rohanyadav75@github.com>
2026-07-21 12:44:32 +0530

    all done

--- src/page/Api.jsx
+++ src/page/Api.jsx
@@ -1,41 +1,75 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+
+import { useEffect, useState } from "react";
+
+// const Api = () => {
+
+//     const [posts, setPosts] = useState([]);
+
+//     useEffect(() => {
+
+//         async function getData() {
+//             const response = await fetch(
+//                 "https://jsonplaceholder.typicode.com/posts"
+//             );
+
+//             const data = await response.json();
+
+//             setPosts(data);
+//         }
+
+//         getData();
+
+//     });
+
+
+//     return (
+//         <div>
+//             <h1>Blog Posts</h1>
+
+//             {posts.map((post) => (
+//                 <div key={post.id}>
+//                     <ul>
+//                         <li ><h2>{post.title}</h2></li>
+//                         {/* <li><h2>{post.body}</h2></li> */}
+//                     </ul>
+//                 </div>
+//             ))}
+
+//         </div>
+//     );
+// };
+
+// export default Api;
+
 
 const Api = () => {
-
     const [posts, setPosts] = useState([]);
-
     useEffect(() => {
-
-        async function getData() {
-            const response = await fetch(
-                "https://jsonplaceholder.typicode.com/posts"
-            );
+        async function rn() {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+            // const response = await fetch("https://api.slingacademy.com/v1/sample-data/blog-posts/");
 
             const data = await response.json();
-
             setPosts(data);
         }
+        rn();
 
-        getData();
-
-    });
-
-
+    })
     return (
-        <div>
-            <h1>Blog Posts</h1>
-
+        <>
+            <h2>Hello</h2>
             {posts.map((post) => (
                 <div key={post.id}>
-                    <ul>
-                        <li ><h2>{post.title}</h2></li>
-                        {/* <li><h2>{post.body}</h2></li> */}
-                    </ul>
+                    <h2>{post.title}</h2>
+                    {/* <p>{post.description}</p> */}
+
                 </div>
             ))}
 
-        </div>
-    );
-};
+        </>
 
+    )
+
+}
 export default Api;
