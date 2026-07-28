8cd48cedf79c8c9bf92f7a82e1a1c1853509e2fc
Rohan Yadav <rohanyadav75@github.com>
2026-07-28 23:59:42 +0530

    api added

--- src/page/Api.jsx
+++ src/page/Api.jsx
@@ -1,75 +1,37 @@
-// import React, { useEffect, useState } from "react";
 
 import { useEffect, useState } from "react";
 
-// const Api = () => {
-
-//     const [posts, setPosts] = useState([]);
-
-//     useEffect(() => {
-
-//         async function getData() {
-//             const response = await fetch(
-//                 "https://jsonplaceholder.typicode.com/posts"
-//             );
-
-//             const data = await response.json();
-
-//             setPosts(data);
-//         }
-
-//         getData();
-
-//     });
-
-
-//     return (
-//         <div>
-//             <h1>Blog Posts</h1>
-
-//             {posts.map((post) => (
-//                 <div key={post.id}>
-//                     <ul>
-//                         <li ><h2>{post.title}</h2></li>
-//                         {/* <li><h2>{post.body}</h2></li> */}
-//                     </ul>
-//                 </div>
-//             ))}
-
-//         </div>
-//     );
-// };
-
-// export default Api;
-
+import React from 'react'
 
 const Api = () => {
-    const [posts, setPosts] = useState([]);
+    const [post, setPosts] = useState([]);
     useEffect(() => {
         async function rn() {
-            const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-            // const response = await fetch("https://api.slingacademy.com/v1/sample-data/blog-posts/");
-
-            const data = await response.json();
-            setPosts(data);
+            // const res = await fetch("https://api.slingacademy.com/v1/sample-data/blog-posts/");
+            const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+            const data = await res.json();
+            setPosts(data)
         }
         rn();
-
     })
     return (
         <>
-            <h2>Hello</h2>
-            {posts.map((post) => (
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    {/* <p>{post.description}</p> */}
+            <h2>Post</h2>
 
-                </div>
+            {post.map((post) => (
+                <ul>
+                    <li>
+                        <div key={post.id}>
+                            <h2>{post.title}</h2>
+                        </div>
+                    </li>
+                </ul>
+
             ))}
 
         </>
-
     )
-
 }
-export default Api;
+
+export default Api
+
